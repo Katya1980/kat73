6bc70262dc19f73dc87211fc8c0dfa356c095772
Lenovo <nikamotors2020@mail.ru>
2023-11-21 14:45:12 +0300

    домашка5 задача 2

---
+++ src/Zadacha2.java
@@ -0,0 +1,25 @@
+
+
+public class Zadacha2 {
+    public static void main(String[] args) {
+
+        String[][] arr = new String[8][8];
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if ((i % 2 == 0)&& (j % 2 == 0) ) {
+                    arr[i][j] = "W"+" ";
+
+                }  else if ( (i % 2 != 0) && (j%2 !=0)) {
+                    arr[i][j] = "W" + " ";
+                }
+                 else {
+                     arr[i][j] = "B" + " ";
+                }
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+
+
+    }
+}
